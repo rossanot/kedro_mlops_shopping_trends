4fbddf4c403a20193c7de81881604356b9183adb
mrt <rossanot@users.noreply.github.com>
2023-11-24 19:42:08 -0700

    Implement eda pipeline and nodes

--- src/kedro_mlops_shopping_trends/pipelines/eda/nodes.py
+++ src/kedro_mlops_shopping_trends/pipelines/eda/nodes.py
@@ -5,7 +5,9 @@ generated using Kedro 0.18.14
 
 import pandas as pd
 import matplotlib.pyplot as plt
-from typing import Any
+import seaborn as sns
+
+from typing import Any, List, Dict
 
 def get_description(dataset: Any) -> pd.DataFrame:
     """Get basic statistical descriptors of the dataset
@@ -18,12 +20,56 @@ def get_description(dataset: Any) -> pd.DataFrame:
     df = pd.read_csv(dataset)
     return df.describe()
 
-def get_corr_plot(df: pd.DataFrame) -> None:
-    """Plot a correlation plot for a given pd.DataFrame
 
-    :param df: _description_
+def _get_categ_features(df: pd.DataFrame) -> List[str]:
+    """Get categorical features
+
+    :param df: A dataframe
     :type df: pd.DataFrame
-    """
+    :return: A list of features of object type
+    """    
+    return df.select_dtypes(include='object').columns.to_list()
 
-    plt.matshow(df.corr())
-    plt.savefig('')
+
+def get_mult_bar_plot(df: pd.DataFrame):
+    """Get bar plot
+
+    :param df: the entire dataframe
+    :type df: pd.DataFrame
+    :return: a matplotlib object
+    """    
+
+    features = _get_categ_features(df)
+    a, b = int(len(features)/3)+1, 3
+    c = 1
+
+    fig = plt.figure(figsize=(25, 55))
+    for feature in features:
+        plt.subplot(a, b, c)
+        plt.barh(list(df[feature].value_counts().index), \
+                    df[feature].value_counts().values)
+        plt.xticks(rotation=45)
+        plt.xlabel('Count')
+        plt.ylabel(feature)
+        c+=1
+
+    return fig
+
+
+def get_box_plot(df: pd.DataFrame, features: Dict) -> List:
+    """Generate box plot
+
+    :param df: the entire dataframe
+    :type df: pd.DataFrame
+    :param features: a Dict containing the features to plot
+    :type features: Dict
+    :return: a list of matplotlib objects
+    """
+    figs = []
+    for feature in features['features']:
+        fig = plt.figure(figsize=(5, 5))
+        sns.boxplot(data=df, x=feature, y='Review Rating', hue=feature)
+        
+        figs.append(fig)
+
+    return figs
--- src/kedro_mlops_shopping_trends/pipelines/eda/pipeline.py
+++ src/kedro_mlops_shopping_trends/pipelines/eda/pipeline.py
@@ -3,8 +3,24 @@ This is a boilerplate pipeline 'eda'
 generated using Kedro 0.18.14
 """
 
-from kedro.pipeline import Pipeline, pipeline
+from kedro.pipeline import Pipeline, pipeline, node
+from .nodes import get_mult_bar_plot, get_box_plot
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline(
+        [
+            node(
+                name='EDA_categorical_variables1',
+                func=get_mult_bar_plot,
+                inputs='shopping_raw',
+                outputs='shopping_raw_hbar',
+            ),
+            node(
+                name='EDA_categorical_variables2',
+                func=get_box_plot,
+                inputs=['shopping_raw', 'params:box_plot'],
+                outputs=['shopping_raw_box_gender', 'shopping_raw_box_category', 'shopping_raw_box_season'],
+            )
+        ]
+        )
