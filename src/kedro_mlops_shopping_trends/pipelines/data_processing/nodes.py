318658c257d4976476dda94e77442899b077fe49
mrt <rossanot@users.noreply.github.com>
2024-03-27 12:58:02 -0600

    Data Processing Implementation
    Implement:
    - Feature Encoding
    - Feature Engineering
    - Data split
    
    Resulting datasets are:
    - 02_intermediate
    - 03_primary
    - 04_feature
    
    X_train, X_val, X_test, y_train, y_val, and y_test
    are generated in this step per each data type and stored for
    its use in 05_model_input
    
    The pipeline has been tested in a Jupyter Notebook

--- src/kedro_mlops_shopping_trends/pipelines/data_processing/nodes.py
+++ src/kedro_mlops_shopping_trends/pipelines/data_processing/nodes.py
@@ -27,12 +27,14 @@ def _set_dtypes(df: pd.DataFrame) -> pd.DataFrame:
 
 def _fix_redudant_frequency(df: pd.DataFrame) -> pd.DataFrame:
     """Clean redundancy in frequency of purchases classes
+    
+    Note: Own interpretation of Quarterly meaning might be wrong
     """
-    clean_replace = {'Fortnightly': 'Bi-Weekly',
-                     'Every 3 Months': 'Quarterly'}
-    
-    df['Frequency of Purchases'] = df['Frequency of Purchases'].replace(clean_replace)
-    
+    clean_replace = {'Every 3 Months': 'Quarterly'}
+
+    df['Frequency of Purchases'] = df['Frequency of Purchases'].replace(
+        clean_replace)
+
     return df
 
 
@@ -46,29 +48,30 @@ def _encode_location(df: pd.DataFrame) -> pd.DataFrame:
     :return: an ecoded pd.DataFrame
     """
     # Northeast
-    region_0 = ['Connecticut', 'Maine', 'Massachusetts', 
-                'New Hampshire', 'Rhode Island', 'Vermont', 
+    region_0 = ['Connecticut', 'Maine', 'Massachusetts',
+                'New Hampshire', 'Rhode Island', 'Vermont',
                 'New Jersey', 'New York', 'Pennsylvania']
     # Midwest
-    region_1 = ['Illinois', 'Indiana', 'Michigan', 
-                'Ohio', 'Wisconsin', 'Iowa', 'Kansas', 
-                'Minnesota', 'Missouri', 'Nebraska', 
+    region_1 = ['Illinois', 'Indiana', 'Michigan',
+                'Ohio', 'Wisconsin', 'Iowa', 'Kansas',
+                'Minnesota', 'Missouri', 'Nebraska',
                 'North Dakota', 'South Dakota']
     # South
     region_2 = ['Delaware', 'Florida', 'Georgia',
                 'Maryland', 'North Carolina', 'South Carolina',
                 'Virginia', 'Washington', 'D.C.', 'West Virginia',
-                'Alabama', 'Kentucky', 'Mississippi', 'Tennessee', 
+                'Alabama', 'Kentucky', 'Mississippi', 'Tennessee',
                 'Arkansas', 'Louisiana', 'Oklahoma', 'Texas']
     # West
-    region_3 = ['Arizona', 'Colorado', 'Idaho', 'Montana', 'Nevada', 
-                'New Mexico', 'Utah', 'Wyoming', 'Alaska', 
+    region_3 = ['Arizona', 'Colorado', 'Idaho', 'Montana', 'Nevada',
+                'New Mexico', 'Utah', 'Wyoming', 'Alaska',
                 'California', 'Hawaii', 'Oregon', 'Washington']
 
+    df['Location_Regions'] = df['Location']
     regions = [region_0, region_1, region_2, region_3]
     for code, region in enumerate(regions):
-        df.loc[df['Location'].isin(region), 'Location'] = 'region_' + str(code)
-
+        df.loc[df['Location_Regions'].isin(region),
+               'Location_Regions'] = 'region_' + str(code)
     return df
 
 
@@ -79,18 +82,24 @@ def _encode_color(df: pd.DataFrame) -> pd.DataFrame:
     :param df: a pd.DataFrame
     :return: encoded pd.DataFrame
     """
-    color_0 = ['Gray', 'White', 'Beige',\
-               'Charcoal', 'Silver', 'Black', 'Brown'] # whites, grays, and black
-    color_1 = ['Red', 'Maroon', 'Orange',\
-               'Gold', 'Yellow', 'Peach'] # reds, yellows, and oranges
-    color_2 = ['Blue', 'Turquoise', 'Teal',\
-               'Indigo', 'Cyan', 'Green', 'Olive'] # blues and greens
-    color_3 = ['Violet', 'Lavender', 'Pink',\
-               'Purple', 'Magenta'] # pinks and purples
-    
+    # whites, grays, and black
+    color_0 = ['Gray', 'White', 'Beige',
+               'Charcoal', 'Silver', 'Black', 'Brown']
+    # reds, yellows, and oranges
+    color_1 = ['Red', 'Maroon', 'Orange',
+               'Gold', 'Yellow', 'Peach']
+    # blues and greens
+    color_2 = ['Blue', 'Turquoise', 'Teal',
+               'Indigo', 'Cyan', 'Green', 'Olive']
+    # pinks and purples
+    color_3 = ['Violet', 'Lavender', 'Pink',
+               'Purple', 'Magenta']
+
+    df['Color_Groups'] = df['Color']
     colors = [color_0, color_1, color_2, color_3]
     for code, color_i in enumerate(colors):
-        df.loc[df['Color'].isin(color_i), 'Color'] = 'color_' + str(code)
+        df.loc[df['Color_Groups'].isin(color_i),
+               'Color_Groups'] = 'color_' + str(code)
 
     return df
 
@@ -113,28 +122,28 @@ def _encode_item_purchased(df: pd.DataFrame) -> pd.DataFrame:
     item_4 = ['Handbag', 'Backpack']  # bags: 4 
     item_5 = ['Dress', 'Skirt']  # dresses: 5
     item_6 = ['Sunglasses', 'Jewelry', 'Belt', 'Socks']  # others: 6
-    
+
+    df['Item_Groups'] = df['Item Purchased']
     items = [item_0, item_1, item_2, item_3, item_4, item_5, item_6]
     for code, item_i in enumerate(items):
-        df.loc[df['Item Purchased'].isin(item_i), 'Item Purchased'] = 'item_' + str(code)
-  
+        df.loc[df['Item_Groups'].isin(item_i),
+               'Item_Groups'] = 'item_' + str(code)
+
     return df
 
 
 def _encode_age(df: pd.DataFrame) -> pd.DataFrame:
     """Encode Age
 
+    Note: cut function implementation inspired by
+    https://www.kaggle.com/code/mehedithedreamer/trendcast-forecasting-shopper-subscription
     :param df: a pd.DataFrame
     :return: encoded pd.DataFrame
     """
-    group_a = df.loc[df['Age'] <= 31].index.to_list()
-    group_b = df.loc[(df['Age'] > 31) & (df['Age'] <= 44)].index.to_list()
-    group_c = df.loc[(df['Age'] > 44) & (df['Age'] <= 57)].index.to_list()
-    group_d = df.loc[df['Age'] > 57].index.to_list()
 
-    groups = [group_a, group_b, group_c, group_d]
-    for code, group in enumerate(groups):
-        df.loc[group, 'Age'] = code
+    df['Age_Groups'] = pd.cut(df['Age'],
+                              bins=[0, 30, 40, 50, 100],
+                              labels=['0-30', '31-40', '41-50', '51+'])
 
     return df
 
@@ -152,9 +161,10 @@ def _encode_purchase_amount(df: pd.DataFrame) -> pd.DataFrame:
                      & (df['Purchase Amount (USD)'] <= 81.)].index.to_list()
     group_d = df.loc[(df['Purchase Amount (USD)'] > 81.)].index.to_list()
 
+    df['Purchase_Groups'] = df['Purchase Amount (USD)']
     groups = [group_a, group_b, group_c, group_d]
     for code, group in enumerate(groups):
-        df.loc[group, 'Purchase Amount (USD)'] = code
+        df.loc[group, 'Purchase_Groups'] = code
 
     return df
 
@@ -172,10 +182,11 @@ def _encode_previous_purchase(df: pd.DataFrame) -> pd.DataFrame:
                      & (df['Previous Purchases'] <= 38)].index.to_list()
     group_d = df.loc[df['Previous Purchases'] > 38].index.to_list()
 
+    df['Previous_Groups'] = df['Previous Purchases']
     groups = [group_a, group_b, group_c, group_d]
     for code, group in enumerate(groups):
-        df.loc[group, 'Previous Purchases'] = code
-    
+        df.loc[group, 'Previous_Groups'] = code
+
     return df
 
 
@@ -189,9 +200,23 @@ def _encode_review_rating(df: pd.DataFrame) -> pd.DataFrame:
     rating_good = df.loc[(df['Review Rating'] > 3.75)
                          & (df['Review Rating'] <= 5.)].index.to_list()
     
-    df.loc[rating_bad, 'Review Rating'] = 0.
-    df.loc[rating_good, 'Review Rating'] = 1.
-    
+    df['Review_Groups'] = df['Review Rating']
+    # Replace
+    df.loc[rating_bad, 'Review_Groups'] = 0.
+    df.loc[rating_good, 'Review_Groups'] = 1.
+
+    return df
+
+
+def _encode_subscription(df: pd.DataFrame) -> pd.DataFrame:
+    """Encode Subscription Status
+
+    :param df: a pd.DataFrame
+    :return: encoded pd.DataFrame
+    """
+    subs = LabelEncoder().fit_transform(df['Subscription Status'])
+    df['Subscription Status'] = subs
+
     return df
 
 
@@ -199,7 +224,7 @@ def _encode_review_rating(df: pd.DataFrame) -> pd.DataFrame:
 # Clean columns
 def get_intermediate(df: pd.DataFrame) -> pd.DataFrame:
     """Encode label and clean features
-    
+
     Encode raw df and generate the intermediate data layer
     :param df: a pd.DataFrame
     :return: encoded pd.DataFrame
@@ -207,18 +232,20 @@ def get_intermediate(df: pd.DataFrame) -> pd.DataFrame:
     _df = df.copy()
     _df = _set_dtypes(_df)
     _df = _fix_redudant_frequency(_df)
-    _df = _encode_review_rating(_df)
+    _df = _encode_subscription(_df)
     return _df
 
 
 def get_primary(df: pd.DataFrame) -> pd.DataFrame:
     """Encode features
-    
+
     Encode raw df and generate the primary data layer
+    Feature engineering: Location, Color, and Item Purchased
     :param df: a pd.DataFrame
     :return: encoded pd.DataFrame
     """
     _df = df.copy()
+    _df = _encode_review_rating(_df)
     _df = _encode_location(_df)
     _df = _encode_color(_df)
     _df = _encode_item_purchased(_df)
@@ -228,8 +255,9 @@ def get_primary(df: pd.DataFrame) -> pd.DataFrame:
 
 def get_feature(df: pd.DataFrame) -> pd.DataFrame:
     """Encode numerical features
-    
+
     Encode raw df and generate the feature data layer
+    Feature engineering: Age, Purchase Amount Previous Purchases
     :param df: a pd.DataFrame
     :return: encoded pd.DataFrame
     """
@@ -237,7 +265,7 @@ def get_feature(df: pd.DataFrame) -> pd.DataFrame:
     _df = _encode_age(_df)
     _df = _encode_purchase_amount(_df)
     _df = _encode_previous_purchase(_df)
-    
+
     return _df
 
 
@@ -249,13 +277,14 @@ def split_data(df: pd.DataFrame, params: Dict, features: Dict) -> Tuple:
     :param params: split data parameters
     :return: test and train pd.DataFrames and pd.Series
     """
+
     X_train, X_test, y_train, y_test = train_test_split(
         df[features['categorical'] + features['numerical']],
         df[features['target']],
         test_size=params['test_size'],
         random_state=params['random_state']
         )
-    
+
     X_val, X_test, y_val, y_test = train_test_split(
         X_test,
         y_test,
@@ -297,9 +326,9 @@ def get_model_input(
             )
         _X_val = _X_val.dropna()
         _X_test = _X_test.dropna()
-    
+
     logger.info('During encoding, were dropped:')
     logger.info('{} rows from X_val and {} rows from X_test'.format(
         X_val.shape[0]-_X_val.shape[0], X_test.shape[0]-_X_test.shape[0]))
-    
+
     return _X_train, _X_val, _X_test
