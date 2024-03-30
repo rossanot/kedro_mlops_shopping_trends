fbcd17f7b8c1421d98013047b4900d03386d477a
mrt <rossanot@users.noreply.github.com>
2024-03-29 19:14:32 -0600

    Implement dashboard  file parsing functions

--- streamlit-entry.py
+++ streamlit-entry.py
@@ -3,11 +3,15 @@ import pandas as pd
 
 import plotly.express as px
 
-from dashboard.utils import (read_file, get_filepath)
+from dashboard.utils import (read_datafile, get_filepath, get_features)
 
 st.title('Shopping Patterns USA Dataset')
 
-raw_df = read_file(get_filepath('shopping_raw'))
+raw_df = read_datafile(get_filepath('shopping_raw'))
 st.write(raw_df.head())
 
+params_inter = get_features('intermediate_data_layer_pipeline')
+categ_inter = params_inter['categorical']
+numeric_inter = params_inter['numerical']
 
+st.write(raw_df[categ_inter])
